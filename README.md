73fb31a4d56bbc61571de0a7118696477cd1fd28
JayVae <919433524@qq.com>
2019-04-10 20:05:57 +0800

    :heavy_plus_sign: 添加观看视频脚本

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. hexo目录下是自动部署hexo博客，点击deploy.bat即可；
 
 2. autoInternet是自动连接校园网，并登陆账号进行验证。
+
+3. CourseRead中是观看课程的脚本，有两种不同的方式。其中，由于通过progress需要带cooki，而浏览器中无法用ajax带cookie，因此只能每次登陆了执行，或通过postman来带cookie执行。
---
+++ courseRead/setByWatchTime.js
@@ -0,0 +1,49 @@
+﻿var ll=$(acontains('去观看'));
+var j=0;
+var i = 0 ;
+
+        function getUrlParam(url,name) {
+            var reg = new RegExp((^&) + name + =([^&])(&$)); 构造一个含有目标参数的正则表达式对象
+            var r = url.match(reg);  匹配目标参数
+            if (r != null) return unescape(r[2]); return null; 返回参数值
+        }
+
+ 发送观看时间
+function timeout(t){
+	$.ajax({ 
+	url'httpxjtudj.edu.cn80coursecourse_updateUserWatchRecord.do',
+	typePOST,
+	data{courseIDcourseID,watchTimet2,ccIDccID,classIDclassID},
+	async  false,
+	dataTypejson,
+	successfunction(data){
+		 console.info(data);
+		 if(data.flag){
+			window.location.href = httpxjtudj.edu.cn80htmlcoursecourse_opened.jsp
+		} 
+	}
+	});
+
+}
+
+for(j=0;jll.length;j++){
+	console.log(ll[j].href);
+	courseID=getUrlParam(ll[j].href,courseID);
+	ccID=getUrlParam(ll[j].href,ccID);
+	classID=getUrlParam(ll[j].href,classID);
+ 	console.log(courseID);
+
+	for(i = 0;i900;i++){
+		setTimeout(timeout(2i),1);
+
+	}
+
+}
+
+
+
+
+setTimeout(function(){
+		
+
+	},10)
